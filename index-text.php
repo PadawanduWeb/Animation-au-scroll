a60d3e60ebc8ae2aa0fcf744f1a13979f8d538a6
PadawanduWeb <padawanduweb@gmail.com>
2014-09-28 18:36:13 +0200

    Update and rename scripting.js to index-text.php

---
+++ index-text.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+<head>
+     <title>Mon Site</title>
+     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+     <link rel="stylesheet" media="screen" type="text/css" href="css/MonSite.css" />
+     <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+     <script type="text/javascript" src="js/jquery.jcarousellite.js"></script>
+     <script type="text/javascript" src="js/jquery.easing.js"></script>
+     <script type="text/javascript" src="js/jquery.mousewheel.js"></script> 
+</head>
+<body>
+<div id="wrapper">
+<h1>Défilement de News avec <span class="grey">jCarousel Lite</span> !</h1>
+<div class="text">
+<ul>
+     <li><img src="images/pic_mini_1.jpg" alt="" />
+	 <h2>Web Design - De Photoshop au HTML</h2>
+	 Ex turba vero imae sortis et paupertinae in tabernis aliqui pernoctant vinariis, non nulli velariis umbraculorum theatralium latent, 
+	 quae Campanam imitatus lasciviam Catulus in aedilitate sua suspendit omnium primus; aut pugnaciter aleis certant turpi sono fragosis naribus introrsum reducto spiritu concrepantes;
+	 aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis, per minutias aurigarum equorumque praecipua vel delicta scrutantes.	 
+	 </li>
+	 <li><img src="images/pic_mini_2.jpg" alt="" />
+	 <h2>Web Design - Réussir son Blog</h2>
+	 Ex turba vero imae sortis et paupertinae in tabernis aliqui pernoctant vinariis, non nulli velariis umbraculorum theatralium latent, 
+	 quae ugnaciter aleis certant turpi sono fragosis naribus introrsum reducto spiritu concrepantes;
+	 aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis, per minutias aurigarum equorumque praecipua vel delicta scrutantes.	 	 
+	 </li>
+	 <li><img src="images/pic_mini_3.jpg" alt="" />
+	 <h2>Web Design - HTML, CSS et Javascript</h2>
+	 Ex turba vero imae sortis et paupertinae in tabernis aliqui pernoctant vinariis, non nulli velariis umbraculorum theatralium latent, 
+	 quae ugnaciter aleis certant turpi sono fragosis naribus introrsum reducto spiritu concrepantes;
+	 aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis, per minupi sono fragosis naribus introrsum reducto spiritu concrepantes;
+	 aut quod est studiorum omnium is, per minutias aurigarum equorumque praecipua vel delicta scrutantes.	 	 
+	 </li>
+	 <li><img src="images/pic_mini_4.jpg" alt="" />
+	 <h2>Web Design - Wordpress3</h2>
+	 Ex turba vero imae sortis et paupertinae in tabernis aliqui pernoctant vinariis, non nulli velariis umbraculorum theatralium latent, 
+	 quae ugnaciter aleis certant turpi sono fragosis naribus introrsum reducto spiritu concrepantes;
+	 aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis, per minupi sono fragosis naribus introrsum reducto spiritu concrepantes;
+	 aut quod est studiorum omnium is, per minutias aurigarum equorumque praecipua vel delicta scrutantes.	 	 
+	 </li>
+	 <li><img src="images/pic_mini_5.jpg" alt="" />
+	 <h2>Web Design - Photoshop et le Webdesign</h2>
+	 Ex turba vero imae sortis et paupertinae in tabernis aliqui pernoctant vinariis, non nulli velariis umbraculorum theatralium latent, 
+	 quae ugnaciter aleis certant turpi sono fragosis naribus introrsum reducto spiritu concrepantes;
+	 aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis, per minupi sono fragosis naribus introrsum reducto spiritu concrepantes;
+	 aut quod est studiorum omnium is, per minutias aurigarum equorumque praecipua vel delicta scrutantes.	 	 
+	 </li>
+</ul>
+</div><br />
+<p><a href="index.html">Voir affichage de photos</a></p>
+</div>
+<script type="text/javascript">
+$(function() {
+        $(".text").jCarouselLite({
+				visible: 2,
+				auto: true,
+				speed: 5000
+          });
+});
+</script>
+</body>
+</html>
